aa634abfaa898ee0983d04800914a386d21c72a8
Nathan Rossow <nathanrossow@gmail.com>
2026-09-05 01:04:51 -0500

    Update delivery comments to include 'Per Month'

--- rsr-plus-scaling-v1.py
+++ rsr-plus-scaling-v1.py
@@ -1,4 +1,3 @@
-# 7000 Packages Delivered
-# 70000 Packages Delivered
-# 95000 Packages Delivered
-
+# 7000 Packages Delivered Per Month
+# 70000 Packages Delivered Per Month
+# 95000 Packages Delivered Per Month
