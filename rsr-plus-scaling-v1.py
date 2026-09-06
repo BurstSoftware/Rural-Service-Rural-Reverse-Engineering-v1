0f9ad3ca98707bcf069d261968673671c040a695
Nathan Rossow <nathanrossow@gmail.com>
2026-09-06 16:40:50 -0500

    Add comments on package delivery details
    Added comments regarding package delivery statistics and dates.

--- rsr-plus-scaling-v1.py
+++ rsr-plus-scaling-v1.py
@@ -1,3 +1,9 @@
 # 7000 Packages Delivered Per Month
 # 70000 Packages Delivered Per Month
 # 95000 Packages Delivered Per Month
+
+# MN7 deilviers 2.45 million pacakges.
+
+# e40ps delivers over 1 million packages.
+
+sept 6th 2026
